a22ba8e09a5b5895238ed56ed2032df88dc9da0e
Support <argserverdev@gmail.com>
2025-09-01 22:35:58 -0300

    feat: Implement product grid and filtering components

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,15 @@
+import { CustomPagination } from '../../../components/custom/CustomPagination';
+import { products } from '../../../mocks/products.mock';
 import { CustomJumbotron } from '../../components/CustomJumbotron';
+import { ProductsGrid } from '../../components/ProductsGrid';
 
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={ products } />
+      <CustomPagination totalPages={ 5 } />
     </>
   );
 };
