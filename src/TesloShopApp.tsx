fd8d52770a2fcfeaadee8ba2ed835db5aba664ee
Tomassini <ltomassini@argentum.com.ar>
2025-09-03 15:17:00 -0300

    docs: complete and structure project README

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,18 +1,16 @@
 import { RouterProvider } from 'react-router';
+import { appRouter } from './app.router';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { appRouter } from './app.router';
-
+const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
-
-  const queryClient = new QueryClient;
-
   return (
-    <QueryClientProvider client={ queryClient }>
-      <RouterProvider router={ appRouter } />;
-      <ReactQueryDevtools initialIsOpen={ false } />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />;
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
